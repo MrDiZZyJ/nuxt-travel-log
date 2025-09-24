2e7d2ba705821c96134d693f46f78fc6bb114c96
MrDiZZyJ <mstayton@gmail.com>
2025-09-24 13:39:46 -0400

    Tl 9 (#48)
    * small change test
    
    Signed-off-by: MrDiZZyJ <mstayton@gmail.com>
    
    * small change test
    
    * :sparkles: add lint workflow
    
    * :bug: add pnpm to action
    
    ---------
    
    Signed-off-by: MrDiZZyJ <mstayton@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: [
+    "@nuxt/eslint",
+  ],
   eslint: {
     config: {
       standalone: false,
